739cd97c5014339e0258ae5545ce514c3f57ad24
Karodips <54810893+Karodips@users.noreply.github.com>
2019-12-03 13:57:49 +0300

    Update N1WIP.cpp

--- N1WIP.cpp
+++ N1WIP.cpp
@@ -5,21 +5,34 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int i, n, nspec, ZapolnyatelMassivaSerii, SchetSerii;
+	int i, n, ZapolnyatelMassivaSerii, SchetSerii;
 	cin >> n;
-	nspec = n;
+	SchetSerii = 0;
 	ZapolnyatelMassivaSerii = 1;
 	int* arr = new int[n];
-	int* seriya = new int[nspec];
+	int* seriya = new int[n];
 	int* chisla = new int[n];
 	for (i = 0; i < n; i++) cin >> arr[i];
 	for (i = 0; i < n; i++) cout << arr[i] << " ";
 	cout << endl;
 	for (i = 0; i < n-1; i++) {
-		if (arr[i] = arr[i + 1]) {
-
+		if (arr[i] == arr[i + 1]) {
+			ZapolnyatelMassivaSerii = ZapolnyatelMassivaSerii + 1;
 		}
+		else {
+			seriya[SchetSerii] = ZapolnyatelMassivaSerii;
+			ZapolnyatelMassivaSerii = 1;
+			SchetSerii++;
+		}
+		chisla[i] = arr[i];
 	}
+	chisla[n - 1] = arr[n - 1];
+	for (i = 0; i < n; i++) {
+		if (seriya[i] = 0) i = n;
+		else cout << seriya[i] << " ";
+	}
+	cout << endl;
+	for (i = 0; i < n; i++) cout << chisla[i] << " ";
 	cout << endl;
 	system("pause");
 	return 0;
